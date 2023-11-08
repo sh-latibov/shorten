d7d08c01d406d1e482ef5ff2e0b58d570b2be0eb
Shamshir <s.latibov@uzinfocom.uz>
2023-11-08 18:30:25 +0500

    check

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sh-latibov/shorten/internal/config"
+	"github.com/sh-latibov/shorten/internal/lib/logger/sl"
+	"github.com/sh-latibov/shorten/internal/storage/sqlite"
+	"golang.org/x/exp/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	_ = storage
+
+	fmt.Println(cfg)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
